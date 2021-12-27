04fb60fc212660c24332d2819337b33bcd7c4a6b
Alwin Esch <alwin.esch@web.de>
2021-12-27 09:28:09 +0100

    API related update

--- src/pingpong.cpp
+++ src/pingpong.cpp
@@ -23,8 +23,8 @@
 CPingPong::CPingPong()
 {
 #ifndef WIN32
-  std::string fraqShader = kodi::GetAddonPath("resources/shaders/" GL_TYPE_STRING "/frag.glsl");
-  std::string vertShader = kodi::GetAddonPath("resources/shaders/" GL_TYPE_STRING "/vert.glsl");
+  std::string fraqShader = kodi::addon::GetAddonPath("resources/shaders/" GL_TYPE_STRING "/frag.glsl");
+  std::string vertShader = kodi::addon::GetAddonPath("resources/shaders/" GL_TYPE_STRING "/vert.glsl");
   if (!LoadShaderFiles(vertShader, fraqShader) || !CompileAndLink())
     return;
   
